8f1bf4ff08ba42fb254c9b02930b28c2a5cff290
Yuliana Banduryst <yuliaaanabanduryst@gmail.com>
2025-04-08 13:12:03 +0300

    Added 4 task project

---
+++ task4/BePresent.Application/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BePresent.Application.DTOs;
+using BePresent.Domain.Users;
+
+namespace BePresent.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User?> RegisterUserAsync(UserRegisterDto dto);
+        Task<User?> LoginUserAsync(UserLoginDto dto);
+    }
+}
