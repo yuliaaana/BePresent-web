d7abe715ab0e33333a1f04eea7ea2f89882112b0
Yuliana Banduryst <yuliaaanabanduryst@gmail.com>
2025-05-20 13:49:16 +0300

    Added final task

--- task4/BePresent.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BePresent.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
